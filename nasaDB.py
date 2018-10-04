f51a4a9a7e68ac9a94f2645b25c30e3fc7dffe51
Shuayb <shuaybpopoola@gmail.com>
2018-10-04 20:52:21 +0100

    working DB

--- nasaDB.py
+++ nasaDB.py
@@ -1,6 +1,11 @@
 #Importing the Mongodb driver for python
 from pymongo import MongoClient
 
+#importing utility to parse environment variables, to be used  in deployment and getting the key
+from envparse import env
+env.read_envfile()
+mongodb_server = env.str('MONGODB_SERVER')
+
 
 class TheClient(object):
 
@@ -9,7 +14,7 @@ class TheClient(object):
         '''connectTimeoutMS  30 s to allow for PaaS warm-up:
         The connection timeout value determines the maximum amount of time the driver will wait for a
         connection to be established with the server. This value is only used when making an initial connection to the
-         database, and so selecting the correct setting for this timeout can be a balancing act.
+         database, considering poor network coverage, so selecting the correct setting for this timeout can be a balancing act.
 
          socketTimeoutMS is set to 5mins:
          The socket timeout option specifies to the driver how long to wait for responses from the server.
@@ -21,14 +26,11 @@ class TheClient(object):
         socketKeepAlive Enabled (True):
          to ensure idle connections are kept alive in the presence of a firewall
          '''
-
-        self.my_client = MongoClient('mongodb://shuayb:shuayb1@ds121373.mlab.com:21373/nasa_db', connectTimeoutMS=30000,
+        self.my_client = MongoClient(mongodb_server, connectTimeoutMS=30000,
                                 socketTimeoutMS=500000, socketKeepAlive=True)
         self.nasa_db = self.my_client.get_default_database()
-        self.nasa_coll = self.nasa_db['nasa_collections']
-        print(self.nasa_db.collection_names())
+        self.nasa_coll_all = self.nasa_db['nasa_collections']
+        self.nasa_coll_today = self.nasa_db['nasa_collections_today']
 
         ### Only close the connection when your app is terminating
-        self.my_client.close()
-
-
+      #  self.my_client.close()
